34aae9c4d85123a0c5e5a2cae1622a7584ca5a55
why <you@example.com>
2020-01-10 12:27:49 +0800

    添加跟新

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 //app.js
 import * as store from './store/index.js';
-import { getData, showToast, globalTabindex, setData} from './utils/util.js';
+import { getData, showToast, globalTabindex, setData } from './utils/util.js';
 App({
   onLaunch: function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    this.checkForUpdate()
     // 登录
     wx.login({
       success: res => {
@@ -16,32 +16,47 @@ App({
       }
     })
     // 获取用户信息
-    if(getData('sessionID'))
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            withCredentials: true,
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-              store.getUserInfo({},(data) => {
-                if(data && data.user) this.globalData.user = data.user;
-              });
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+    if (getData('sessionID'))
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            wx.getUserInfo({
+              withCredentials: true,
+              success: res => {
+                // 可以将 res 发送给后台解码出 unionId
+                this.globalData.userInfo = res.userInfo
+                store.getUserInfo({}, (data) => {
+                  if (data && data.user) this.globalData.user = data.user;
+                });
+                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                // 所以此处加入 callback 以防止这种情况
+                if (this.userInfoReadyCallback) {
+                  this.userInfoReadyCallback(res)
+                }
               }
-            }
-          })
+            })
+          }
         }
-      }
-    })
+      })
   },
   globalData: {
     userInfo: null,
     user: null
+  },
+  checkForUpdate: function () {
+    const updateManager = wx.getUpdateManager()
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+    updateManager.onUpdateFailed(function () { })
   }
 })
