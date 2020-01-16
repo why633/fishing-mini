d4787daace02b6917da5189f05030c33252e2cca
why <you@example.com>
2020-01-16 17:28:46 +0800

    去除积分tab

--- pages/sellFish/sellFish.js
+++ pages/sellFish/sellFish.js
@@ -1,17 +1,26 @@
 import QRCode from '../../utils/weapp-qrcode.js'
-import { getData, showToast } from '../../utils/util.js'
+import { getData, showToast, encodeUtf8 } from '../../utils/util.js'
 Page({
   data: {
   },
   onLoad: function () {
+    // 判断是否授权登录
     if (!getData('sessionID')) {
       showToast("登录状态错误，请绑定微信后再试", 'none');
       return wx.switchTab({
         url: '../me/me'
       })
     }
+    // 判断是否绑定手机号
+    if (!getData('userInfo').phone) {
+      showToast("账户状态错误，请绑定手机再报名", 'none');
+      return wx.navigateTo({
+        url: '../../me/bind/bind'
+      })
+    }
+    const qrCodeData = encodeUtf8(`${getData('userInfo').id}#${getData('userInfo').headImg}#${getData('userInfo').nickName}`)
     new QRCode('myQrcode', {
-      text: getData('userInfo').id,
+      text: qrCodeData,
       width: 200,
       height: 200,
       padding: 0, // 生成二维码四周自动留边宽度，不传入默认为0
--- utils/util.js
+++ utils/util.js
@@ -136,6 +136,27 @@ function adaptPadding(self) {
   })
 }
 
+// utf-8编码
+function encodeUtf8(str) {
+  var out, i, len, c;
+  out = "";
+  len = str.length;
+  for(i = 0; i < len; i++) {
+    c = str.charCodeAt(i);
+    if ((c >= 0x0001) && (c <= 0x007F)) {
+      out += str.charAt(i);
+    } else if (c > 0x07FF) {
+      out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
+      out += String.fromCharCode(0x80 | ((c >>  6) & 0x3F));
+      out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+    } else {
+      out += String.fromCharCode(0xC0 | ((c >>  6) & 0x1F));
+      out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+    }
+  }
+  return out;
+}
+
 module.exports = {
   setData,
   adaptPadding,
@@ -149,5 +170,6 @@ module.exports = {
   link,
   isLogin,
   dialPhone,
-  getDateNum
+  getDateNum,
+  encodeUtf8
 }
